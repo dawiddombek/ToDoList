0783c42965c0d7a8f2ed9aae21539ff9c80951d7
Dawid <dawid.DMK@interia.pl>
2023-04-22 21:44:59 +0200

    Added controller and service class

---
+++ src/main/java/com/ToDoList/controller/ToDoListController.java
@@ -0,0 +1,24 @@
+package com.ToDoList.controller;
+
+import com.ToDoList.servise.ToDoListService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class ToDoListController {
+
+    private final ToDoListService toDoListService;
+
+    @Autowired
+    public ToDoListController(ToDoListService toDoListService) {
+        this.toDoListService = toDoListService;
+    }
+
+    @GetMapping("/")
+    public String getHomePage() {
+        return "index";
+    }
+}
