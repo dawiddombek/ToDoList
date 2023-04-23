82432f7190d7cb0a6182f873ca1931f61eb86417
Dawid <dawid.DMK@interia.pl>
2023-04-23 16:48:23 +0200

    Added new features to the site. There is a problem with css file

--- src/main/java/com/ToDoList/controller/ToDoListController.java
+++ src/main/java/com/ToDoList/controller/ToDoListController.java
@@ -4,6 +4,7 @@ import com.ToDoList.servise.ToDoListService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -20,5 +21,15 @@ public class ToDoListController {
     @GetMapping("/")
     public String getHomePage() {
         return "index";
+    }
+
+    @PostMapping("/AddNewTask")
+    public String postAddNewTask() {
+        return "redirect:/test";
+    }
+
+    @GetMapping("/test")
+    public String getTest() {
+        return "test";
     }
 }
